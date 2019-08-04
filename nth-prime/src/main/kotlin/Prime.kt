fd6065d801c4c06de9ec348f1978cbdea64136b3
sakemi <satoshi.sakemi@gmail.com>
2019-08-04 17:21:33 +0900

    Implemented nth prime.

--- nth-prime/src/main/kotlin/Prime.kt
+++ nth-prime/src/main/kotlin/Prime.kt
@@ -1,5 +1,42 @@
+import java.lang.ArithmeticException
+import java.lang.IllegalArgumentException
+
 object Prime {
+    private val cache = arrayListOf(2)
+    private var biggestPrime = 2
+
+    @Throws(IllegalArgumentException::class)
     fun nth(n: Int): Int {
-        return 0
+        if (n == 0) {
+            throw IllegalArgumentException("There is no zeroth prime.")
+        }
+        if (n < 0) {
+            throw IllegalArgumentException("The positive number should be given. Given: $n")
+        }
+
+        if (n <= cache.size) {
+            return cache[n - 1]
+        }
+
+        for (i in biggestPrime + 1..Int.MAX_VALUE) {
+            if (isPrime(i)) {
+                cache.add(i)
+                biggestPrime = i
+                if (cache.size == n) {
+                    return i
+                }
+            }
+        }
+
+        throw ArithmeticException("${n}th prime is too big to calculate.")
+    }
+
+    private fun isPrime(num: Int): Boolean {
+        for (prime in cache) {
+            if (num % prime == 0) {
+                return false
+            }
+        }
+        return true
     }
 }
--- nth-prime/src/test/kotlin/PrimeTest.kt
+++ nth-prime/src/test/kotlin/PrimeTest.kt
@@ -1,5 +1,4 @@
 import org.junit.Test
-import org.junit.Ignore
 import org.junit.Rule
 import org.junit.rules.ExpectedException
 import kotlin.test.assertEquals
@@ -15,25 +14,25 @@ class PrimeTest {
         assertEquals(2, Prime.nth(1))
     }
 
-    @Ignore
+    
     @Test
     fun secondPrime() {
         assertEquals(3, Prime.nth(2))
     }
 
-    @Ignore
+    
     @Test
     fun sixthPrime() {
         assertEquals(13, Prime.nth(6))
     }
 
-    @Ignore
+    
     @Test
     fun bigPrime() {
         assertEquals(104743, Prime.nth(10001))
     }
 
-    @Ignore
+    
     @Test
     fun undefinedPrime() {
         expectedException.expect(IllegalArgumentException::class.java)
